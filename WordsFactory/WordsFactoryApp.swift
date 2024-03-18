78b2ca3b31e646d35a31e4ad41f0f50e192ffbbc
Ivan Semenov <devsemtsk@gmail.com>
2024-03-18 13:24:18 +0700

    Init

---
+++ WordsFactory/WordsFactoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsFactoryApp.swift
+//  WordsFactory
+//
+//  Created by Ivan Semenov on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordsFactoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
