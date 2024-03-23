857013ca48530c7d731851fc9372ff81899d0ae2
Ivan Semenov <devsemtsk@gmail.com>
2024-03-24 02:18:48 +0700

    Add auth entities

---
+++ WordsFactory/Sources/Features/Auth/Sources/Auth/Domain/Entities/LoginCredentials.swift
@@ -0,0 +1,11 @@
+//
+//  LoginCredentials.swift
+//
+//
+//  Created by Ivan Semenov on 24.03.2024.
+//
+
+struct LoginCredentials {
+    let email: String
+    let password: String
+}
---
+++ WordsFactory/Sources/Features/Auth/Sources/Auth/Domain/Entities/UserRegister.swift
@@ -0,0 +1,12 @@
+//
+//  UserRegister.swift
+//
+//
+//  Created by Ivan Semenov on 24.03.2024.
+//
+
+struct UserRegister {
+    let name: String
+    let email: String
+    let password: String
+}
