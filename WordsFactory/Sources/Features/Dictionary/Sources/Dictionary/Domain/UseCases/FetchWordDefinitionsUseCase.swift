8ac1f5f78152f72a4b32a6d89b503a41c1dd396d
Ivan Semenov <devsemtsk@gmail.com>
2024-03-26 12:41:28 +0700

    Add FetchWordDefinitionsUseCase

---
+++ WordsFactory/Sources/Features/Dictionary/Sources/Dictionary/Domain/UseCases/FetchWordDefinitionsUseCase.swift
@@ -0,0 +1,19 @@
+//
+//  FetchWordDefinitionsUseCase.swift
+//
+//
+//  Created by Ivan Semenov on 26.03.2024.
+//
+
+final class FetchWordDefinitionsUseCase {
+
+    private let wordDefinitionRepository: WordDefinitionRepository
+
+    init(wordDefinitionRepository: WordDefinitionRepository) {
+        self.wordDefinitionRepository = wordDefinitionRepository
+    }
+
+    func execute(_ word: String) async throws -> [WordDefinition] {
+        try await wordDefinitionRepository.getDefinitions(of: word)
+    }
+}
