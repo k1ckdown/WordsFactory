5c1d899baff81c1081786dde59b1aef20e08a5ef
Ivan Semenov <devsemtsk@gmail.com>
2024-03-30 13:19:41 +0700

    Generate core data models

---
+++ WordsFactory/Sources/Features/Dictionary/Sources/Dictionary/Data/CoreData/Models/MeaningDefinition/CDMeaningDefinition+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDMeaningDefinition+CoreDataClass.swift
+//  WordsFactory
+//
+//  Created by Ivan Semenov on 30.03.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDMeaningDefinition)
+public class CDMeaningDefinition: NSManagedObject {
+
+}
---
+++ WordsFactory/Sources/Features/Dictionary/Sources/Dictionary/Data/CoreData/Models/WordDefinition/CDWordDefinition+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDWordDefinition+CoreDataClass.swift
+//  WordsFactory
+//
+//  Created by Ivan Semenov on 30.03.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDWordDefinition)
+public class CDWordDefinition: NSManagedObject {
+
+}
