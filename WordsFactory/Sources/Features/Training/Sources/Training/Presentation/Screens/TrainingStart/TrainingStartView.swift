cd0cbab5c43e8c1aa1fd7e81b6f973993862b718
Ivan Semenov <devsemtsk@gmail.com>
2024-04-01 02:00:03 +0700

    Refactor TrainingStartView

--- WordsFactory/Sources/Features/Training/Sources/Training/Presentation/Screens/TrainingStart/TrainingStartView.swift
+++ WordsFactory/Sources/Features/Training/Sources/Training/Presentation/Screens/TrainingStart/TrainingStartView.swift
@@ -9,20 +9,20 @@ import SwiftUI
 import CommonUI
 
 struct TrainingStartView: View {
-    
+
     @StateObject private var viewModel: TrainingStartViewModel
-    
+
     init(viewModel: TrainingStartViewModel) {
         _viewModel = StateObject(wrappedValue: viewModel)
     }
-    
+
     var body: some View {
         contentView
             .onAppear {
                 viewModel.handle(.onAppear)
             }
     }
-    
+
     @ViewBuilder
     private var contentView: some View {
         switch viewModel.state {
@@ -32,45 +32,57 @@ struct TrainingStartView: View {
             loadedView(viewData)
         }
     }
-    
-    private func loadedView(_ viewData: TrainingStartViewModel.ViewState.ViewData) -> some View {
+}
+
+// MARK: - Loaded view
+
+private extension TrainingStartView {
+
+    func loadedView(_ viewData: TrainingStartViewModel.ViewState.ViewData) -> some View {
         VStack(spacing: .zero) {
-            VStack(spacing: .zero) {
-                HighlightedText(
-                    Strings.dictionaryWords(viewData.totalWords),
-                    color: Colors.appOrange.swiftUIColor
-                )
-                
-                Text("\(Strings.startTraining)?")
-                    .padding(.top, Constants.subtitleInsetTop)
-            }
-            .font(Fonts.headline4)
-            .multilineTextAlignment(.center)
-            .padding(.horizontal, Constants.titlesHorizontalInset)
-            
-            Group {
-                if let countdownDuration = viewData.countdownDuration {
-                    CountdownView(duration: countdownDuration) {
-                        viewModel.handle(.countdownUpdated($0))
-                    }
-                } else {
-                    Button(Strings.start) {
-                        viewModel.handle(.startTapped)
-                    }
-                    .mainButtonStyle()
-                    .padding(.horizontal, Constants.startButtonHorizontalInset)
-                }
-            }
-            .frame(height: Constants.countdownHeight)
-            .padding(.top, Constants.countdownTopInset)
+            titlesView(totalWords: viewData.totalWords)
+            countdownView(duration: viewData.countdownDuration)
         }
+    }
+
+    func titlesView(totalWords: Int) -> some View {
+        VStack(spacing: .zero) {
+            HighlightedText(
+                Strings.dictionaryWords(totalWords),
+                color: Colors.appOrange.swiftUIColor
+            )
+
+            Text("\(Strings.startTraining)?")
+                .padding(.top, Constants.subtitleInsetTop)
+        }
+        .font(Fonts.headline4)
+        .multilineTextAlignment(.center)
+        .padding(.horizontal, Constants.titlesHorizontalInset)
+    }
+
+    func countdownView(duration: Double?) -> some View {
+        Group {
+            if let duration {
+                CountdownView(duration: duration) {
+                    viewModel.handle(.countdownUpdated($0))
+                }
+            } else {
+                Button(Strings.start) {
+                    viewModel.handle(.startTapped)
+                }
+                .mainButtonStyle()
+                .padding(.horizontal, Constants.startButtonHorizontalInset)
+            }
+        }
+        .frame(height: Constants.countdownHeight)
+        .padding(.top, Constants.countdownTopInset)
     }
 }
 
 // MARK: - Constants
 
 private extension TrainingStartView {
-    
+
     enum Constants {
         static let subtitleInsetTop: CGFloat = 35
         static let titlesHorizontalInset: CGFloat = 50
