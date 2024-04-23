d3210ec353d7f968d6402a5e87ccf681836e7651
Ivan Semenov <devsemtsk@gmail.com>
2024-04-23 12:08:10 +0700

    Add CDUserWords

---
+++ WordsFactory/Sources/Shared/WordModule/Sources/WordModule/Data/Models/CoreData/UserWords/CDUserWords+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDUserWords+CoreDataClass.swift
+//  WordsFactory
+//
+//  Created by Ivan Semenov on 23.04.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDUserWords)
+public class CDUserWords: NSManagedObject {
+
+}
--- WordsFactory/Sources/Shared/WordModule/Sources/WordModule/Data/Models/CoreData/Word/CDWord+CoreDataProperties.swift
+++ WordsFactory/Sources/Shared/WordModule/Sources/WordModule/Data/Models/CoreData/Word/CDWord+CoreDataProperties.swift
@@ -19,6 +19,7 @@ extension CDWord {
     @NSManaged public var studyCoefficient: Int64
     @NSManaged public var text: String
     @NSManaged public var definitions: NSSet
+    @NSManaged public var users: NSSet?
 
 }
 
@@ -36,6 +37,23 @@ extension CDWord {
 
     @objc(removeDefinitions:)
     @NSManaged public func removeFromDefinitions(_ values: NSSet)
+
+}
+
+// MARK: Generated accessors for users
+extension CDWord {
+
+    @objc(addUsersObject:)
+    @NSManaged public func addToUsers(_ value: CDUserWords)
+
+    @objc(removeUsersObject:)
+    @NSManaged public func removeFromUsers(_ value: CDUserWords)
+
+    @objc(addUsers:)
+    @NSManaged public func addToUsers(_ values: NSSet)
+
+    @objc(removeUsers:)
+    @NSManaged public func removeFromUsers(_ values: NSSet)
 
 }
 
