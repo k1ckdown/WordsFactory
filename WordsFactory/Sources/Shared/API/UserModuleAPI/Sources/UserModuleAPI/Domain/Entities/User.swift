02e294d54aba2e758557f97a45609fb9f4048a18
Ivan Semenov <devsemtsk@gmail.com>
2024-04-23 10:48:21 +0700

    Add Domain to UserModuleAPI

---
+++ WordsFactory/Sources/Shared/API/UserModuleAPI/Sources/UserModuleAPI/Domain/Entities/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//
+//
+//  Created by Ivan Semenov on 23.04.2024.
+//
+
+struct User {
+    let id: String
+    let name: String
+    let email: String
+}
---
+++ WordsFactory/Sources/Shared/API/UserModuleAPI/Sources/UserModuleAPI/Domain/Repositories/UserRepositoryProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  UserRepositoryProtocol.swift
+//
+//
+//  Created by Ivan Semenov on 23.04.2024.
+//
+
+protocol UserRepositoryProtocol: AnyObject {
+    func getUser() async throws -> User
+    func getUserId() async throws -> String
+}
