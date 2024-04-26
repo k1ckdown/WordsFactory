f6b4ae9952705d9db563553eda6b7a3fe1ec5f97
Ivan Semenov <devsemtsk@gmail.com>
2024-04-27 05:31:40 +0700

    Update domain packages description

--- Shared/Domain/AuthDomain/Package.swift
+++ Shared/Domain/AuthDomain/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AuthDomain",
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "AuthDomain",
--- Shared/Domain/UserDomain/Package.swift
+++ Shared/Domain/UserDomain/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "UserDomain",
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "UserDomain",
