bef7b0854db35fc3f4366e5fd8b9782e263d2c1d
Ivan Semenov <devsemtsk@gmail.com>
2024-04-27 04:54:46 +0700

    Add loaded user to repository

--- Shared/Data/UserData/Sources/UserData/Repository/UserRepository.swift
+++ Shared/Data/UserData/Sources/UserData/Repository/UserRepository.swift
@@ -9,6 +9,7 @@ import UserDomain
 
 final class UserRepository {
 
+    private var loadedUser: User?
     private let remoteDataSource: UserRemoteDataSource
 
     init(remoteDataSource: UserRemoteDataSource) {
@@ -24,18 +25,28 @@ extension UserRepository: UserRepositoryProtocol {
         try remoteDataSource.fetchUserId()
     }
 
-    func getUser() async throws -> User {
-        let userDto = try await remoteDataSource.fetchUser()
-        return userDto.toDomain()
-    }
-
     func saveUser(_ user: User) async throws {
         let userDto = user.toDto()
         try await remoteDataSource.saveUser(userDto)
+        loadedUser = user
     }
 
-    func updateUser(_ user: UserEdit) async throws {
-        let userDto = user.toDto()
-        try await remoteDataSource.updateUser(userDto)
+    func updateUser(_ userEdit: UserEdit) async throws {
+        guard let loadedUser else { return }
+
+        let userEditDto = userEdit.toDto()
+        try await remoteDataSource.updateUser(userEditDto)
+
+        self.loadedUser = User(id: loadedUser.id, name: userEdit.name, email: userEdit.email, joinDate: loadedUser.joinDate)
+    }
+
+    func getUser() async throws -> User {
+        if let loadedUser { return loadedUser }
+
+        let userDto = try await remoteDataSource.fetchUser()
+        let user = userDto.toDomain()
+
+        loadedUser = user
+        return user
     }
 }
